cba2f421f0e75317940a881b244a20a6986c8442
Yehuda Hason <yehudaxt@gmail.com>
2026-06-15 17:43:18 +0300

    header and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,29 @@
 import Footer2 from "./Footer2";
 
 export default function Footer() {
-  // const baseUrl = import.meta.env.BASE_URL;
+  const baseUrl = import.meta.env.BASE_URL;
 
   return (
-    <footer className="footer">
+    <footer className="footer flex flex-col items-center justify-center max-w-360 mx-auto">
+      <div
+        className="flex w-full
+      sm:flex-row 
+      flex-col
+      gap-6
+      items-center justify-between max-w-300"
+      >
+        <span
+          className="flex items-center  justify-center text-preset-9
+        text-green-950"
+        >
+          Made with &nbsp; ❤️ and &nbsp; 🥑
+        </span>
+        <span className="flex items-center justify-center gap-4">
+          <img src={baseUrl + "/assets/images/icon-instagram.svg"} alt="" />
+          <img src={baseUrl + "/assets/images/icon-tiktok.svg"} alt="" />
+          <img src={baseUrl + "/assets/images/icon-bluesky.svg"} alt="" />
+        </span>
+      </div>
       <Footer2 />
     </footer>
   );
